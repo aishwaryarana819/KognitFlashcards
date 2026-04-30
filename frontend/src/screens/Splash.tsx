78abd21f22e537a3e4954e99c01a1dc8573256f0
Aishwarya <aishwaryarana2807@gmail.com>
2026-04-30 06:52:38 +0530

    Added animation to splash screen for better ux

--- frontend/src/screens/Splash.tsx
+++ frontend/src/screens/Splash.tsx
@@ -1,14 +1,34 @@
-import {View, StyleSheet} from 'react-native';
+import {useEffect, useRef} from 'react';
+import {View, StyleSheet, Animated} from 'react-native';
 import {Branding} from "../components/Branding";
 import {useTheme} from "../context/ThemeContext";
 
-// const activePalette = isDark ? darkPalette : lightPalette;
-
 export const Splash = () => {
     const {activePalette} = useTheme();
+    const fadeAnim = useRef(new Animated.Value(0.4)).current;
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(fadeAnim, {
+                    toValue: 1,
+                    duration: 750,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(fadeAnim, {
+                    toValue: 0.2,
+                    duration: 750,
+                    useNativeDriver: true,
+                })
+            ])
+        ).start();
+    }, [fadeAnim]);
+
     return (
         <View style={[styles.container, {backgroundColor: activePalette.bg}]}>
-            <Branding />
+            <Animated.View style={{ opacity: fadeAnim }}>
+                <Branding />
+            </Animated.View>
         </View>
     );
 };
